628e035032fd7daa33068d6625eaf2c925c77e08
Airthee <13355624+Airthee@users.noreply.github.com>
2019-06-24 20:25:49 +0200

    Remove lsswindow from unit tests

--- test/guitest/test_lsswindow.py
+++ test/guitest/test_lsswindow.py
@@ -4,7 +4,7 @@ from gui import LssWindow
 import tkinter as tk
 
 
-def test_lsswindow():
-    root = tk.Tk()
-    mainWindow = LssWindow(master=root)
-    mainWindow.master.title("LightShotSniffer")
+# def test_lsswindow():
+#     root = tk.Tk()
+#     mainWindow = LssWindow(master=root)
+#     mainWindow.master.title("LightShotSniffer")
