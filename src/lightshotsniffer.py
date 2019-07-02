964ffb67b8cda76d681c787fbb64f6bb1b345105
Airthee <13355624+Airthee@users.noreply.github.com>
2019-07-02 23:19:22 +0200

    fix #6

--- src/lightshotsniffer.py
+++ src/lightshotsniffer.py
@@ -1,6 +1,7 @@
 # *-* coding: utf8
 
 import sys
+import argparse
 from tools import Generator
 from tools import Downloader
 from gui import LssWindow
@@ -8,29 +9,42 @@ import tkinter as tk
 
 
 def main():
-    # By default, withGui is true
-    withGui = getArgumentValue("--gui", "True").upper() in ["TRUE", "YES", "OK", "1"]
-
-    # CLI mode
-    if not withGui:
-        # Run sniffer
-        startVal = getArgumentValue("--start", "zzzzzz")
-        endVal = getArgumentValue("--end", "000000")
-        runSniffer(startVal, endVal)
+    args = parse_arguments()
 
     # GUI mode
-    else:
+    if args.gui:
         # Lancement de la fenêtre
         root = tk.Tk()
         mainWindow = LssWindow(master=root)
         mainWindow.master.title("LightShotSniffer")
         mainWindow.mainloop()
-        print("C'est partit")
+
+    # CLI mode
+    else:
+        # Run sniffer
+        startVal = args.start if args.start else "zzzzzz"
+        endVal = args.end if args.end else "000000"
+        run_sniffer(startVal, endVal)
 
     return True
 
+def parse_arguments():
+    # Parse arguments
+    parser = argparse.ArgumentParser()
 
-def runSniffer(start, end):
+    # GUI group
+    groupGui = parser.add_argument_group("Run with GUI")
+    groupGui.add_argument("--gui", help="Run programm with GUI", action="store_true", default=False)
+
+    # CLI group
+    groupCli = parser.add_argument_group("Run with CLI (default)")
+    groupCli.add_argument("--start", help="id to start from", default="zzzzzz")
+    groupCli.add_argument("--end", help="id to end to", default="000000")
+
+    return parser.parse_args()
+
+
+def run_sniffer(start, end):
     # For each value
     # Get url and ask to keep or not the image
     gen = Generator(start, end)
@@ -44,15 +58,6 @@ def runSniffer(start, end):
         if not gen.next():
             break
     return 0
-
-
-def getArgumentValue(argName, defaultValue=None):
-    try:
-        argIndex = sys.argv.index(argName)
-        return sys.argv[argIndex + 1]
-    except ValueError:
-        return defaultValue
-
 
 if __name__ == "__main__":
     main()
--- test/test_lightshotsniffer.py
+++ test/test_lightshotsniffer.py
@@ -3,17 +3,10 @@ from lightshotsniffer import *
 from unittest.mock import patch
 import sys
 
-
-def test_getArgumentValue():
-    assert getArgumentValue("--myarg", "defaultVal") == "defaultVal"
-    assert getArgumentValue("--myarg2") == None
-
-
-def test_runSniffer():
-    assert runSniffer("o3ka62", "o3ka62") == 0
-
+def test_run_sniffer():
+    assert run_sniffer("o3ka62", "o3ka62") == 0
 
 def test_main():
-    testargs = [__file__, "--gui", "False", "--start", "o3ka62", "--end", "o3ka62"]
+    testargs = [__file__, "--start", "o3ka62", "--end", "o3ka62"]
     with patch.object(sys, "argv", testargs):
         assert main() == True
